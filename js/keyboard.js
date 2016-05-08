0c2822abeaa94630b04ea0b42700ff6e78904c91
akosua kernizan <akernizan@gmail.com>
2016-05-08 17:53:47 -0400

    WIP color change

--- js/keyboard.js
+++ js/keyboard.js
@@ -87,9 +87,59 @@ function playlist() {
 	var i = 0;
 
 	setInterval(function () {
+		//		changeBackground();
 		var audio = melody[i++];
 		audio.play();
 
 		if (i == melody.length) i = 0;
 	}, 1000);
+}
+
+
+function changeBackground() {
+	for (var i = 0; i < melody.length; i++) {
+		if (melody[i].id == 'audio / a_note.mp3') {
+			$('#one').css({
+				backgroundColor: 'rgba(255, 0, 0, 1)'
+			})
+		}
+
+		if (melody[i].src == 'audio/b_note.mp3') {
+			$('#two').css({
+				backgroundColor: 'rgba(255, 165, 0, 1)'
+			})
+		}
+
+		if (melody[i].src == 'audio/c_note.mp3') {
+			$('#three').css({
+				backgroundColor: 'rgba(255, 255, 0, 1)'
+			})
+		}
+
+		if (melody[i].src == 'audio/d_note.mp3') {
+			$('#four').css({
+				backgroundColor: 'rgba(0, 128, 0, 1)'
+			})
+		}
+
+		if (melody[i].src == 'audio/e_note.mp3') {
+			$('#five').css({
+				backgroundColor: 'rgba(0, 0, 255, 1)'
+			})
+		}
+
+		if (melody[i].src == 'audio/f_note.mp3') {
+			$('#six').css({
+				backgroundColor: 'rgba(128, 0, 128, 1)'
+			})
+		}
+
+		if (melody[i].src == 'audio/g_note.mp3') {
+			$('#seven').css({
+				backgroundColor: 'rgba(255, 192, 203, 1)'
+			})
+		}
+
+
+	}
 }
